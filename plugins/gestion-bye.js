171dbb6780a2e5bbe1356ae7d01d10121cf906a9
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-12-05 21:10:38 -0500

    Actualizar gestion-bye.js

--- plugins/gestion-bye.js
+++ plugins/gestion-bye.js
@@ -8,8 +8,8 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 
 // Función para obtener nombre y estandarte del bot según la sesión/config
 function getBotConfig(conn) {
-  let nombreBot = typeof botname !== 'undefined' ? botname : 'Yotsuba Nakano'
-  let bannerFinal = 'https://qu.ax/zRNgk.jpg'
+  let nombreBot = typeof botname !== 'undefined' ? botname : 'Waguri Bot'
+  let bannerFinal = 'https://raw.githubusercontent.com/ANDERSONARRUE/Img.2/main/upload_1764986859271.jpg'
 
   const botActual = conn.user?.jid?.split('@')[0]?.replace(/\D/g, '')
   const configPath = join('./JadiBots', botActual || '', 'config.json')

5b335c67f56c809bac512b28c0021c83ad75aca5
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-11 11:29:11 -0500

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -1,5 +1,4 @@
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
-
+// by Duartexv
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
 
@@ -8,39 +7,37 @@ var handler = async (m, { conn, usedPrefix }) => {
 if (global.conn.user.jid !== conn.user.jid) {
 return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m)
 }
-await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m)
-m.react(rwait)
 
+let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
 let sessionPath = `./${sessions}/`
 
 try {
 
-if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, `${emoji} La carpeta está vacía.`, m)
-}
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
-for (const file of files) {
-if (file !== 'creds.json') {
+for (let file of files) {
+for (let id of chatId) {
+if (file.includes(id.split('@')[0])) {
 await fs.unlink(path.join(sessionPath, file))
 filesDeleted++;
-}
-}
-if (filesDeleted === 0) {
-await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m)
-} else {
-m.react(done)
-await conn.reply(m.chat, `${emoji} Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.`, m)
-conn.reply(m.chat, `${emoji} *¡Hola! ¿logras verme?*`, m)
+break
+}}}
 
+if (filesDeleted === 0) {
+await conn.reply(m.chat, `${emoji2} No se encontró ningún archivo que incluya la ID del chat.`, m)
+} else {
+await conn.reply(m.chat, `${emoji2} Se eliminaron ${filesDeleted} archivos de sesión.`, m)
+conn.reply(m.chat, `${emoji} ¡Hola! ¿logras verme?`, m)
 }
 } catch (err) {
-console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m)
+console.error('Error al leer la carpeta o los archivos de sesión:', err)
+await conn.reply(m.chat, `${emoji} Hola Soy ${botname} Sigue El Canal y apoyanos porfavor.\n\n> ${channel}`, m)
 }
 
 }
-handler.help = ['dsowner']
-handler.tags = ['owner']
-handler.command = ['delai', 'dsowner', 'clearallsession']
-handler.rowner = true;
+handler.help = ['ds', 'fixmsgespera']
+handler.tags = ['info']
+handler.command = ['fixmsgespera', 'ds']
+handler.register = true
+
+export default handler
